6b70de932b7112a98e3260f0e0c923042d59168c
Sunglok Choi <sunglok@hanmail.net>
2022-01-13 08:49:13 +0900

    Encapsulate 'myAHRSPlus' with multi-threading

--- ahrs_test.py
+++ ahrs_test.py
@@ -1,40 +1,77 @@
-import serial
+import serial, threading, time
 
-#!python -m serial.tools.list_ports
+class myAHRSPlus:
+    def __init__(self):
+        self.__dev = serial.Serial()
+        self.seq = 0
+        self.xyzw = [0., 0., 0., 1.]
+        self.__lock = threading.Lock()
+        self.__thread = threading.Thread(target=self.recv_data)
+        self.__thread_exit = False
 
-def make_cmd(command):
-    if command:
-        crc = 0
-        for b in command:
-            crc = crc ^ b
-        return command + b'*' + ('%02X' % crc).encode() + b'\r\n'
-    return b''
+    def __del__(self):
+        self.close()
 
-try:
-    ahrs = serial.Serial('COM4', 115200, timeout=1)
-    if ahrs.is_open:
+    def open(self, port, baudrate=115200, timeout=1):
+        if not self.__dev.is_open:
+            self.__dev.port = port
+            self.__dev.baudrate = baudrate
+            self.__dev.timeout = timeout
+            self.__dev.open()
+            if self.__dev.is_open:
+                self.__dev.reset_input_buffer()
+                self.send_cmd(b'@mode,AT')
+                self.send_cmd(b'@asc_out,QUAT')
+                self.send_cmd(b'@divider,1')
+                self.send_cmd(b'@mode,AC')
+                self.__thread_exit = False
+                self.__thread.start()
+                return True
+        return False
+
+    def is_open(self):
+        return self.__dev.is_open
+
+    def close(self, timeout=10):
+        self.__thread_exit = True
+        for trial in range(1, 100):
+            if not self.__thread.is_alive():
+                break
+            time.sleep(timeout / 100.)
+        self.__dev.close()
+
+    def get_xyzw(self):
+        with self.__lock:
+            return list(self.xyzw) # Return the copy
+
+    def send_cmd(self, command):
+        if command:
+            crc = 0
+            for b in command:
+                crc = crc ^ b
+            packet = command + b'*' + ('%02X' % crc).encode() + b'\r\n'
+            self.__dev.write(packet)
+            self.__dev.read_until()
+        return False
+
+    def recv_data(self):
+        while not self.__thread_exit:
+            packet = self.__dev.read_until()
+            token = packet.split(b',')
+            if len(token) == 6:
+                with self.__lock:
+                    self.seq = int(token[1])
+                    self.xyzw = [float(token[2]), float(token[3]), float(token[4]), float(token[5].split(b'*')[0])]
+            self.__dev.reset_input_buffer()
+
+
+
+if __name__ == '__main__':
+    ahrs = myAHRSPlus()
+    if ahrs.open('COM4'):
         try:
-            ahrs.reset_input_buffer()
-            ahrs.write(make_cmd(b'@mode,AT'))
-            ahrs.read_until()
-            ahrs.write(make_cmd(b'@asc_out,QUAT'))
-            ahrs.read_until()
-            ahrs.write(make_cmd(b'@divider,1'))
-            ahrs.read_until()
-            ahrs.write(make_cmd(b'@mode,AC'))
-            ahrs.read_until()
             while True:
-                buffer = ahrs.read_until()
-                packet = buffer.split(b',')
-                if len(packet) == 6:
-                    seq = int(packet[1])
-                    qx = float(packet[2])
-                    qy = float(packet[3])
-                    qz = float(packet[4])
-                    qw = float(packet[5].split(b'*')[0])
-                    print(f'XYZW: {qx:.2f}, {qy:.2f}, {qz:.2f}, {qw:.2f}')
-                ahrs.reset_input_buffer()
-        except:
+                print(f'XYZW: {ahrs.get_xyzw()}')
+                time.sleep(0.1)
+        except KeyboardInterrupt:
             ahrs.close()
-except:
-    print('Cannot open the port')
