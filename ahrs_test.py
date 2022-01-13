c4df78e9dde2106db16879c4f9224000663e11bd
Sunglok Choi <sunglok@hanmail.net>
2022-01-13 11:21:22 +0900

    Visualize AHRS data

--- ahrs_test.py
+++ ahrs_test.py
@@ -1,10 +1,17 @@
+import numpy as np
 import serial, threading, time
+from scipy.spatial.transform import Rotation
+import pyvista as pv
+
+rad2deg = lambda rad: rad * 180 / np.pi
+deg2rad = lambda deg: deg * np.pi / 180
 
 class myAHRSPlus:
-    def __init__(self):
+    def __init__(self, verbose=True):
         self.__dev = serial.Serial()
         self.seq = 0
         self.xyzw = [0., 0., 0., 1.]
+        self.__verbose = verbose
         self.__lock = threading.Lock()
         self.__thread = threading.Thread(target=self.recv_data)
         self.__thread_exit = False
@@ -24,9 +31,14 @@ class myAHRSPlus:
                 self.send_cmd(b'@asc_out,QUAT')
                 self.send_cmd(b'@divider,1')
                 self.send_cmd(b'@mode,AC')
-                self.__thread_exit = False
                 self.__thread.start()
                 return True
+            else:
+                if self.__verbose:
+                    print('[myAHRSPlus] Cannot open the port, {port}')
+        else:
+            if self.__verbose:
+                print('[myAHRSPlus] The port, {port}, is already open.')
         return False
 
     def is_open(self):
@@ -55,6 +67,9 @@ class myAHRSPlus:
         return False
 
     def recv_data(self):
+        self.__thread_exit = False
+        if self.__verbose:
+            print("[myAHRSPlus] Start 'recv_data' thread.")
         while not self.__thread_exit:
             packet = self.__dev.read_until()
             token = packet.split(b',')
@@ -62,16 +77,32 @@ class myAHRSPlus:
                 with self.__lock:
                     self.seq = int(token[1])
                     self.xyzw = [float(token[2]), float(token[3]), float(token[4]), float(token[5].split(b'*')[0])]
+            else:
+                if self.__verbose:
+                    print(f'[myAHRSPlus] Receive an wrong packet, {str(packet)}')
             self.__dev.reset_input_buffer()
+        print("[myAHRSPlus] Terminate 'recv_data' thread.")
 
 
 
 if __name__ == '__main__':
     ahrs = myAHRSPlus()
     if ahrs.open('COM4'):
+        plotter = pv.Plotter()
+        plotter.add_axes_at_origin('r', 'g', 'b')
+        ahrs_mesh = pv.Cube(x_length=0.8, y_length=0.4, z_length=0.004)
+        ahrs_actor = plotter.add_mesh(ahrs_mesh, color='r')
+        plotter.set_viewup([0, 0, 1])
+        plotter.show(title='SeoulTech AHRS Visualization', interactive_update=True)
+
         try:
             while True:
-                print(f'XYZW: {ahrs.get_xyzw()}')
-                time.sleep(0.1)
+                q_xyzw = ahrs.get_xyzw()
+                y, x, z = Rotation.from_quat(q_xyzw).as_euler('yxz', degrees=True)
+                ahrs_actor.SetOrientation(x, y, z)
+                plotter.update()
         except KeyboardInterrupt:
-            ahrs.close()
+            pass
+
+        plotter.close()
+        ahrs.close()
