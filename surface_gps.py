36a21351f4986834c41119f21a17184e780072eb
Sunglok Choi <sunglok@hanmail.net>
2022-01-16 01:32:09 +0900

    Add 'SurfaceGPS' on the sphere

--- surface_gps.py
+++ surface_gps.py
@@ -1,27 +1,62 @@
 import numpy as np
+from scipy.spatial.transform import Rotation
 import pyvista as pv
-import time
+from ahrs import myAHRSPlus, AHRSCube
 
 rad2deg = lambda rad: rad * 180 / np.pi
+deg2rad = lambda deg: deg * np.pi / 180
+
+class SphereSurface:
+    def __init__(self, plotter, radius=1., color='gray', opacity=0.5):
+        self.radius = radius
+        self.mesh = pv.Sphere(radius=radius)
+        self.actor = plotter.add_mesh(self.mesh, color=color, opacity=opacity)
+
+    def normal2position(self, q_xyzw):
+        R = Rotation.from_quat(q_xyzw)
+        return self.radius * R.as_matrix()[:,-1]
+
+class SurfaceGPS:
+    def __init__(self, surface):
+        self.p = [0, 0, 0]
+        self.q = [0, 0, 0, 1]
+        self.surface = surface
+
+    def get_pose(self):
+        return (self.p, self.q)
+
+    def apply_ahrs(self, q_xyzw):
+        self.p = self.surface.normal2position(q_xyzw)
+        self.q = q_xyzw
+        return True
+
+
 
 if __name__ == '__main__':
-    # Prepare
-    sphere_radius = 1
-    plotter = pv.Plotter()
-    axes = pv.Axes(show_actor=True, actor_scale=1.5, line_width=4)
-    plotter.add_actor(axes.actor)
-    sphere = pv.Sphere(radius=sphere_radius)
-    sphere_actor = plotter.add_mesh(sphere)
-    ahrs = pv.Cube(x_length=0.2, y_length=0.1, z_length=0.001)
-    ahrs_actor = plotter.add_mesh(ahrs, color='r')
+    # Open the VHRS device
+    ahrs_dev = myAHRSPlus()
+    if ahrs_dev.open('COM4'):
 
-    # Move 'ahrs' on 'sphere'
-    plotter.show(interactive_update=True)
-    for theta in np.linspace(0, 2 * np.pi, 100):
-        x = sphere_radius * np.cos(theta)
-        y = sphere_radius * np.sin(theta)
-        ahrs_actor.SetPosition(x, y, 0)
-        ahrs_actor.SetOrientation(0, 90, rad2deg(theta))
-        plotter.update()
-        time.sleep(0.1)
-    plotter.show()
+        # Instantiate objects
+        plotter = pv.Plotter()
+        surface = SphereSurface(plotter)
+        ahrs_viz = AHRSCube(plotter, scale=10)
+        localizer = SurfaceGPS(surface)
+
+        # Configure visualization
+        plotter.add_axes_at_origin('r', 'g', 'b')
+        plotter.show(title='SeoulTech SurfaceGPS', interactive_update=True)
+
+        # Localize the AHRS and show it
+        try:
+            while True:
+                q_xyzw = ahrs_dev.get_xyzw()
+                localizer.apply_ahrs(q_xyzw)
+                p, q = localizer.get_pose()
+                ahrs_viz.set_position(p)
+                ahrs_viz.set_orientation(q)
+                plotter.update()
+        except KeyboardInterrupt:
+            pass
+
+        plotter.close()
