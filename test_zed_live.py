0fa77f6cd7a97f5854f340b73e1448899d763761
Hyunkil_Hwang <hyunkil76@gmail.com>
2022-06-15 02:57:48 +0900

    need check

---
+++ test_zed_live.py
@@ -0,0 +1,26 @@
+import cv2
+import pyzed.sl as sl
+
+init = sl.InitParameters()
+cam = sl.Camera()
+if not cam.is_opened():
+    print("Opening ZED Camera...")
+
+status = cam.open(init)
+
+runtime = sl.RuntimeParameters()
+mat = sl.Mat()
+
+key = ''
+while key != 113:  # for 'q' key
+    err = cam.grab(runtime)
+    if err == sl.ERROR_CODE.SUCCESS:
+        cam.retrieve_image(mat, sl.VIEW.LEFT)
+        cv2.imshow("ZED", mat.get_data())
+        key = cv2.waitKey(5)
+
+    else:
+        key = cv2.waitKey(5)
+cv2.destroyAllWindows()
+
+cam.close()
