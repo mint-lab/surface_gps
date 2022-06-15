6bbbc0aacf7cb886134b9c79a6dc61a973be87a0
Sunglok Choi <sunglok@hanmail.net>
2022-06-15 09:48:34 +0900

    Revise code little bit

--- test_zed_svo.py
+++ test_zed_svo.py
@@ -2,8 +2,13 @@ import cv2 as cv
 import pyzed.sl as sl
 
 if __name__ == '__main__':
+    svo_file = 'data/zed_MR327_220614.svo'
+    svo_realtime = False
+
     # Instantiate the camera object
-    init = sl.InitParameters()
+    input_type = sl.InputType()
+    input_type.set_from_svo_file(svo_file)
+    init = sl.InitParameters(input_t=input_type, svo_real_time_mode=svo_realtime)
     cam = sl.Camera()
     status = cam.open(init)
     if status != sl.ERROR_CODE.SUCCESS:
@@ -12,6 +17,7 @@ if __name__ == '__main__':
 
     # Prepare to grab images
     runtime = sl.RuntimeParameters()
+    runtime.sensing_mode = sl.SENSING_MODE.FILL
     rgb_img = sl.Mat()
     depth_img = sl.Mat()
 
