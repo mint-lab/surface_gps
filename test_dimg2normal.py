8c80e095a4a1867f6597a5d7c3fd99cae83a0c32
Hyunkil76 <hyunkil76@gmail.com>
2023-02-12 02:13:32 +0900

    Add floodfill

--- test_dimg2normal.py
+++ test_dimg2normal.py
@@ -1,17 +1,59 @@
 import numpy as np
 import cv2 as cv
+from sensorpy.zed import ZED
+from time import time
 
-d_v = np.load('Gym_depth.npy')*255
+ret_list = []
 
-grad_x = (d_v[:,2:]-d_v[:,:-2])/2
-grad_y = (d_v[2:]-d_v[:-2])/2
-direction = np.dstack((-grad_x[1:-1], -grad_y[:,1:-1], np.ones_like(grad_x[1:-1])))
-magnitude = np.sqrt((direction**2).sum(axis=2))
-magnitude = np.dstack((magnitude, magnitude, magnitude))
-normal = direction/magnitude
+def onMouse(event, x, y, flags, param) :
+    global mask, normal
+    if event == cv.EVENT_LBUTTONDOWN:
+        seed = (x,y)
+        retval = cv.floodFill(normal, mask, seed, newVal, loDiff, upDiff)
+        ret_list.append(retval)
+        cv.imshow('normal', normal)
 
-cv.imshow('depth', d_v)
-cv.imshow('normal', np.abs(normal))
 
-cv.waitKey(0)
+newVal = (1,1,1)
+
+loDiff, upDiff = (0.01,0.01,0.01), (0.01,0.01,0.01)
+
+zed = ZED()
+zed.open(svo_file='data/230116_M327/auto_v.svo', min_depth=0.2, max_depth = 20)
+# zed.open(min_depth=0.2, max_depth = 20)
+
+while zed.is_open():
+    if zed.grab():
+        s_time = time()
+        d_v = zed.get_depth()*1000
+        color,_,_ = zed.get_images()
+        norm_dv = d_v-d_v.min()
+        norm_dv = norm_dv/d_v.max()
+        
+        #Increase step -> Decrease noise, Make contour
+        grad_x = (d_v[:,20:]-d_v[:,:-20])/20
+        grad_y = (d_v[20:]-d_v[:-20])/20
+        
+        direction = np.dstack((-grad_x[10:-10], -grad_y[:,10:-10], norm_dv[10:-10,10:-10]))
+        
+        magnitude = np.sqrt((direction**2).sum(axis=2))
+        magnitude = np.dstack((magnitude, magnitude, magnitude))
+        
+        normal = (direction/magnitude+1)/2          #Make -1~1 scale to 0~1 scale,   num<0.5 : minus
+        print(time()-s_time)
+        
+        rows, cols = normal.shape[:2]
+        mask = np.zeros((rows+2, cols+2), np.uint8)
+        
+        cv.imshow('normal', normal)
+        cv.imshow('color', color)
+        cv.setMouseCallback('normal', onMouse)
+        
+        key = cv.waitKey(1)
+        if key == ord(' '):
+            key = cv.waitKey(0)
+        if key == 27:
+            break;
+    
 cv.destroyAllWindows()
+zed.close()
