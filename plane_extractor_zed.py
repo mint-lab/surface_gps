6ed1f62dd66f33307d25d6b435c242eda6b86eaf
Sunglok Choi <sunglok@hanmail.net>
2023-02-12 09:59:10 +0900

    Separate RANSAC-based methods from ZED-based methods

--- plane_extractor_zed.py
+++ plane_extractor_zed.py
@@ -1,28 +1,87 @@
 import numpy as np
 import cv2 as cv
+import open3d as o3d
 import opencx as cx
 from sensorpy.zed import ZED, sl
-import open3d as o3d
 import time
-from scipy.linalg import null_space
-import random
 
-class ZEDPlaneExtractor:
-    def __init__(self, zed, length_threshold=0.1, area_threshold=0.1, x_n = 8, y_n = 4):
+class ZEDSinglePlane:
+    def __init__(self, zed, seed_pt=None):
+        self.zed = zed
+        self.zed_plane = None
+
+        # Get the image resolution and the camera matrix
+        zed_info = zed.camera.get_camera_information()
+        self.zed_width = zed_info.camera_resolution.width
+        self.zed_height = zed_info.camera_resolution.height
+        cam = zed_info.calibration_parameters.left_cam
+        self.zed_K = np.array([[cam.fx,      0, cam.cx],
+                               [     0, cam.fy, cam.cy],
+                               [     0,      0,      1]])
+
+        # Initialize the default seed point
+        if seed_pt is not None:
+            self.seed_pt = seed_pt
+        else:
+            self.seed_pt = (self.zed_width / 2, self.zed_height / 2)
+
+    def extract(self, depth=None, seed_pt=None):
+        self.zed_plane = None
+        if seed_pt is None:
+            seed_pt = self.seed_pt
+        plane = sl.Plane()
+        status = self.zed.camera.find_plane_at_hit(seed_pt, plane)
+        if status == sl.ERROR_CODE.SUCCESS:
+            self.zed_plane = plane
+            return plane.get_plane_equation().reshape(1, -1)
+        return np.array([])
+
+    def get_plane_image(self, depth=None, seed_pt=None, seed_pt_radius=10, seed_pt_thickness=2, seed_pt_color=(0, 255, 255)):
+        image = np.zeros((self.zed_height, self.zed_width, 3), dtype=np.uint8)
+
+        if self.zed_plane is not None:
+            # Draw the plane
+            bounds_meter = self.zed_plane.get_bounds()
+            bounds_pixel = bounds_meter @ self.zed_K.T
+            bounds_pixel = bounds_pixel[:,0:2] / bounds_pixel[:,-1].reshape(-1, 1)
+            rgb = (255 * np.abs(self.zed_plane.get_normal())).astype(np.uint8)
+            cv.fillPoly(image, [bounds_pixel.astype(np.int32)], rgb[::-1].tolist())
+
+        if seed_pt_radius > 0:
+            # Draw 'seed_pt'
+            if seed_pt is None:
+                seed_pt = self.seed_pt
+            seed_pt = np.array(seed_pt).astype(np.int32)
+            cv.line(image, seed_pt-(seed_pt_radius,0), seed_pt+(seed_pt_radius,0), thickness=seed_pt_thickness, color=seed_pt_color)
+            cv.line(image, seed_pt-(0,seed_pt_radius), seed_pt+(0,seed_pt_radius), thickness=seed_pt_thickness, color=seed_pt_color)
+
+        return image
+
+    def get_plane_mesh(self, depth=None, seed_pt=None):
+        if self.zed_plane is not None:
+            mesh = self.zed_plane.extract_mesh()
+            mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(mesh.vertices.astype(np.float64)),
+                                             triangles=o3d.utility.Vector3iVector(mesh.triangles.astype(np.int32)))
+            mesh.paint_uniform_color(np.abs(self.zed_plane.get_normal()))
+            return [mesh]
+        return []
+
+class ZEDMultiPlane:
+    def __init__(self, zed, length_threshold=0.1, area_threshold=0.1, dist_threshold=0.1, seed_x_n=8, seed_y_n=4):
         self.zed = zed
         self.zed_planes = []
         self.length_threshold = length_threshold
         self.area_threshold = area_threshold
+        self.dist_threshold = dist_threshold
 
-        # Generate the sampling points based on the image resolution
+        # Generate the seed points based on the image resolution
         zed_info = zed.camera.get_camera_information()
         self.zed_width = zed_info.camera_resolution.width
         self.zed_height = zed_info.camera_resolution.height
-        x_p = np.linspace(0, self.zed_width, x_n+2).astype(np.int32)[1:-1]
-        y_p = np.linspace(0, self.zed_height, y_n+2).astype(np.int32)[1:-1]
-        xx, yy = np.meshgrid(x_p, y_p)
-        yx = np.dstack((yy, xx))
-        self.sample_pts = yx.reshape(-1, 2).astype(np.float32)
+        xs = np.linspace(0, self.zed_width,  seed_x_n+2, dtype=np.int32)[1:-1]
+        ys = np.linspace(0, self.zed_height, seed_y_n+2, dtype=np.int32)[1:-1]
+        xx, yy = np.meshgrid(xs, ys)
+        self.seed_pts = np.dstack((xx, yy)).reshape(-1, 2)
 
         # Generate the camera matrix
         cam = zed_info.calibration_parameters.left_cam
@@ -30,118 +89,51 @@ class ZEDPlaneExtractor:
                                [     0, cam.fy, cam.cy],
                                [     0,      0,      1]])
 
-    def extract(self, depth=None, get_meshs=False):
+    def extract(self, depth=None):
         self.zed_planes = []
-        self.zed_meshs = []
-
         plane_eqs = []
-        center_arrs = np.empty((0,3))
-        for pt in self.sample_pts:
-                plane = sl.Plane()
-                mesh = sl.Mesh()
-                find_plane_status = self.zed.camera.find_plane_at_hit(pt, plane)
-                if str(find_plane_status) == 'SUCCESS' and np.max(plane.get_extents())> self.length_threshold and np.prod(plane.get_extents())>self.area_threshold:
-                    if (center_arrs==np.round(plane.get_center(),2)).any(axis=1).any() == False:
-                        center_arrs = np.vstack((center_arrs, np.round(plane.get_center(),2)))
-                        bounds_m = plane.get_bounds()
-                        bounds_px = bounds_m @ self.zed_K.T
-                        bounds_px = bounds_px / bounds_px[:,-1].reshape(-1, 1)
-                        self.zed_planes.append((plane, bounds_px[:,0:2].astype(np.float32)))
-                        plane_eqs.append(plane.get_plane_equation())
-                        if get_meshs:
-                            mesh = plane.extract_mesh()
-                            mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(mesh.vertices.astype(np.float64)), triangles=o3d.utility.Vector3iVector(mesh.triangles.astype(np.int32)))
-                            mesh.paint_uniform_color(np.abs(plane.get_plane_equation()[:3]))
-                            self.zed_meshs.append(mesh)
-        if get_meshs:
-            return np.vstack(plane_eqs), self.zed_meshs
+        center_pts = np.empty((0, 3))
+        for seed_pt in self.seed_pts:
+            plane = sl.Plane()
+            status = self.zed.camera.find_plane_at_hit(seed_pt, plane)
+            if status == sl.ERROR_CODE.SUCCESS and np.max(plane.get_extents())> self.length_threshold and np.prod(plane.get_extents())>self.area_threshold:
+                center_pt = plane.get_center()
+                delta = center_pts - center_pt.reshape(-1, 3)
+                dist = np.linalg.norm(delta, axis=1)
+                if np.all(dist > self.dist_threshold):
+                    plane_eqs.append(plane.get_plane_equation())
+                    center_pts = np.vstack((center_pts, center_pt))
+                    self.zed_planes.append(plane)
         return np.vstack(plane_eqs)
 
-    def get_normal_image(self, depth=None):
+    def get_plane_image(self, depth=None, seed_pt_radius=10, seed_pt_thickness=2, seed_pt_color=(0, 255, 255)):
         image = np.zeros((self.zed_height, self.zed_width, 3), dtype=np.uint8)
-        for plane, polygon2d in self.zed_planes:
+
+        for plane in self.zed_planes:
+            # Draw each plane
+            bounds_meter = plane.get_bounds()
+            bounds_pixel = bounds_meter @ self.zed_K.T
+            bounds_pixel = bounds_pixel[:,0:2] / bounds_pixel[:,-1].reshape(-1, 1)
             rgb = (255 * np.abs(plane.get_normal())).astype(np.uint8)
-            cv.fillPoly(image, [polygon2d.astype(np.int32)], rgb.tolist())
+            cv.fillPoly(image, [bounds_pixel.astype(np.int32)], rgb[::-1].tolist())
+
+        if seed_pt_radius > 0:
+            # Draw each seed point
+            for seed_pt in self.seed_pts:
+                cv.line(image, seed_pt-(seed_pt_radius,0), seed_pt+(seed_pt_radius,0), thickness=seed_pt_thickness, color=seed_pt_color)
+                cv.line(image, seed_pt-(0,seed_pt_radius), seed_pt+(0,seed_pt_radius), thickness=seed_pt_thickness, color=seed_pt_color)
+
         return image
-    
-class RansacExtractor:
-    def __init__(self, length_threshold = 0.1, plane_candidate_n = 1000, plane_n = 3):
-        self.length_threshold = length_threshold
-        self.plane_candidate_n = plane_candidate_n
-        self.plane_n = plane_n
-        
-    def find_plane(self, data):
-        null_v_dic = {}
-        points_dic = {}
-        pcds = []
-        
-        null_coeff = np.empty((0, 4))
 
-        ran_1 = np.zeros(self.plane_candidate_n)
-        ran_2 = np.zeros(self.plane_candidate_n)
-        ran_3 = np.zeros(self.plane_candidate_n)
-
-        r_list = [10, 3, 1]
-        while np.any(ran_1 == ran_2) or np.any(ran_1 == ran_3) or np.any(ran_3 == ran_2):
-            ran_1 = np.random.randint(low=0, high=np.shape(data)[0]-1, size=self.plane_candidate_n, dtype=int)
-            ran_2 = np.random.randint(low=0, high=np.shape(data)[0]-1, size=self.plane_candidate_n, dtype=int)
-            ran_3 = np.random.randint(low=0, high=np.shape(data)[0]-1, size=self.plane_candidate_n, dtype=int)
-        
-        ran_pix = np.vstack((ran_1, ran_2, ran_3)).T
-        
-        for points in ran_pix:
-            p1, p2, p3 = points[0], points[1], points[2]
-            matrix = np.array([data[p1],
-                               data[p2],
-                               data[p3]])
-            matrix = np.hstack((matrix, np.array([[1],[1],[1]])))
-            A, B, C, D = np.squeeze(null_space(matrix))
-            null_coeff = np.vstack((null_coeff, [A, B, C, D]))
-        
-        for n_plane in range(self.plane_n):
-            coeff_arr = null_coeff.copy()      
-            
-            for it in range(3):
-                if it<2:
-                    s_data = data[random.sample(range(0, np.shape(data)[0]), np.shape(data)[0]//(10**(3-it))),:]
-                else:
-                    s_data = data
-                coeff_arr = self.count_points(s_data, coeff_arr, r_list[it])
-        
-            A, B, C, D = coeff_arr[0]
-            x = data[:,0]
-            y = data[:,1]
-            z = data[:,2]
-            h = np.abs(A*x+B*y+C*z+D)/np.sqrt(A**2+B**2+C**2)
-            inner_index = h<self.length_threshold
-        
-            points_dic[n_plane] = data[inner_index]
-            null_v_dic[n_plane] = coeff_arr[0]
-            pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(points_dic[n_plane].astype(np.float64)))
-            pcd.paint_uniform_color(np.abs(coeff_arr[0][:-1]))
-            pcds.append(pcd)
-            data = data[~inner_index]
-        
-        return null_v_dic, pcds
-    
-    def count_points(self, s_datas, coeff_arr, r_num):
-        result = np.empty((0,5))
-        x = s_datas[:,0]
-        y = s_datas[:,1]
-        z = s_datas[:,2]
-        
-        for enum in coeff_arr:
-            A, B, C, D = enum
-            h = abs(A*x+B*y+C*z+D)/np.sqrt(A**2+B**2+C**2)
-            inner_index = h<self.length_threshold
-            in_n = inner_index[np.where(inner_index == True)].size
-            result=np.vstack((result, [in_n, A, B, C, D]))
-            
-        result = np.squeeze(result[[result[:, 0].argsort()[::-1]]])
-        
-        return result[:r_num, 1:]
-        
-    
+    def get_plane_mesh(self, depth=None):
+        meshes = []
+        for plane in self.zed_planes:
+            mesh = plane.extract_mesh()
+            mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(mesh.vertices.astype(np.float64)),
+                                             triangles=o3d.utility.Vector3iVector(mesh.triangles.astype(np.int32)))
+            mesh.paint_uniform_color(np.abs(plane.get_normal()))
+            meshes.append(mesh)
+        return meshes
 
 def test_plane_extractor(svo_file='', svo_realtime=False, output_file=''):
     # Open input and output videos
@@ -151,7 +143,7 @@ def test_plane_extractor(svo_file='', svo_realtime=False, output_file=''):
         output = cx.VideoWriter(output_file)
 
     # Run the line segment extractor
-    extractor = ZEDPlaneExtractor(zed)
+    extractor = ZEDMultiPlane(zed)
     while True:
         # Grab an image
         if not zed.grab():
@@ -165,7 +157,7 @@ def test_plane_extractor(svo_file='', svo_realtime=False, output_file=''):
 
         # Visualize its result
         gray = cv.cvtColor(cv.cvtColor(color, cv.COLOR_BGR2GRAY), cv.COLOR_GRAY2BGR) # Make the image gray to highlight result
-        normal = extractor.get_normal_image(depth)
+        normal = extractor.get_plane_image(depth)
         result = cv.addWeighted(gray, 0.5, normal, 0.5, 0)
         cx.putText(result, f'{1/max(elapse, 0.001):.1f} Hz, Planes: {len(planes)}', (10, 10))
         cv.imshow('test_plane_extractors', result)
@@ -186,4 +178,6 @@ def test_plane_extractor(svo_file='', svo_realtime=False, output_file=''):
 
 
 if __name__ == '__main__':
-    test_plane_extractor('data/230116_M327/auto_v.svo')
+    test_plane_extractor('data/220720_M327/short.svo')
+    # test_plane_extractor('data/220902_Gym/short.svo')
+    # test_plane_extractor('data/230116_M327/auto_v.svo')
