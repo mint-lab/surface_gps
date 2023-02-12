7c47a5a75e93c4aefd8d991b62a4e7ae6df4cb47
Hyunkil76 <hyunkil76@gmail.com>
2023-02-12 11:57:41 +0900

    Add Seed points

--- test_dimg2normal.py
+++ test_dimg2normal.py
@@ -3,15 +3,14 @@ import cv2 as cv
 from sensorpy.zed import ZED
 from time import time
 
-ret_list = []
-
-def onMouse(event, x, y, flags, param) :
-    global mask, normal
-    if event == cv.EVENT_LBUTTONDOWN:
-        seed = (x,y)
-        retval = cv.floodFill(normal, mask, seed, newVal, loDiff, upDiff)
-        ret_list.append(retval)
-        cv.imshow('normal', normal)
+# def onMouse(event, x, y, flags, param) :
+#     global mask, normal
+#     if event == cv.EVENT_LBUTTONDOWN:
+#         breakpoint()
+#         seed = (x,y)
+#         retval = cv.floodFill(normal, mask, seed, newVal, loDiff, upDiff)
+#         ret_list.append(retval)
+#         cv.imshow('normal', normal)
 
 
 newVal = (1,1,1)
@@ -20,7 +19,19 @@ loDiff, upDiff = (0.01,0.01,0.01), (0.01,0.01,0.01)
 
 zed = ZED()
 zed.open(svo_file='data/230116_M327/auto_v.svo', min_depth=0.2, max_depth = 20)
+# zed.open(svo_file='data/220902_Gym/short.svo', min_depth=0.2, max_depth = 20)
 # zed.open(min_depth=0.2, max_depth = 20)
+
+zed_info = zed.camera.get_camera_information()
+width = zed_info.camera_resolution.width
+height = zed_info.camera_resolution.height
+seed_x_n=8
+seed_y_n=4
+
+xs = np.linspace(0, width,  seed_x_n+2, dtype=np.int32)[1:-1]
+ys = np.linspace(0, height, seed_y_n+2, dtype=np.int32)[1:-1]
+xx, yy = np.meshgrid(xs, ys)
+seed_pts = np.dstack((xx, yy)).reshape(-1, 2)
 
 while zed.is_open():
     if zed.grab():
@@ -43,11 +54,24 @@ while zed.is_open():
         print(time()-s_time)
         
         rows, cols = normal.shape[:2]
-        mask = np.zeros((rows+2, cols+2), np.uint8)
+        
+        
+        label_arr = np.zeros_like(normal[:,:,0])
+        ret_list = []
+        
+        for pts in seed_pts:
+            if label_arr[pts[1], pts[0]] == 0:
+                mask = np.zeros((rows+2, cols+2), np.uint8)
+                retval = cv.floodFill(normal, mask, pts, newVal, loDiff, upDiff)
+                if retval[0] > 1000:
+                    ret_list.append(retval)
+                    label_arr[(mask==1)[1:-1,1:-1]] = len(ret_list)
+        
         
         cv.imshow('normal', normal)
         cv.imshow('color', color)
-        cv.setMouseCallback('normal', onMouse)
+        cv.imshow('labeling', label_arr/label_arr.max())
+        
         
         key = cv.waitKey(1)
         if key == ord(' '):
