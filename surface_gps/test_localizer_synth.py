e9145bc4c0e6232b776ab689f429d099ad7e7bac
Sunglok Choi <sunglok@hanmail.net>
2024-06-03 18:09:23 +0900

    FEAT: Generalize data generation

--- surface_gps/test_localizer_synth.py
+++ surface_gps/test_localizer_synth.py
@@ -1,19 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
 from simple_localizer import SimpleLocalizer
 
 
-def gen_straight_line(length=10, speed=0.1, time_step=0.5):
+def conjugate(q_xyzw:np.array) -> np.array:
+    return np.array([-q_xyzw[0], -q_xyzw[1], -q_xyzw[2], q_xyzw[3]])
+
+
+def hamilton_product(q1:np.array, q2:np.array) -> np.array:
+    return np.array([
+        q1[3]*q2[0] + q1[0]*q2[3] + q1[1]*q2[2] - q1[2]*q2[1],
+        q1[3]*q2[1] - q1[0]*q2[2] + q1[1]*q2[3] + q1[2]*q2[0],
+        q1[3]*q2[2] + q1[0]*q2[1] - q1[1]*q2[0] + q1[2]*q2[3],
+        q1[3]*q2[3] - q1[0]*q2[0] - q1[1]*q2[1] - q1[2]*q2[2]])
+
+
+def hamilton_rotate(q_xyzw:np.array, v_xyz:np.array) -> np.array:
+    return hamilton_product(hamilton_product(q_xyzw, np.array([v_xyz[0], v_xyz[1], v_xyz[2], 0])), conjugate(q_xyzw))[:3]
+
+
+def make_cv_trajectory(length=100, timestep=1, v=1, w=0, p_init=[0, 0, 0], euler_init=[0, 0, 0]):
+    # Calculate the displacement from the constant linear and angular velocity
+    if type(v) is float or type(v) is int:
+        v = [v, 0, 0] # Assume the 1D velocity is along the x-axis
+    if type(w) is float or type(w) is int:
+        w = [0, 0, w] # Assume the 1D angular velocity is around the z-axis
+    vdt = np.array(v) * timestep
+    wdt = np.array(w) * timestep
+    wdt = R.from_euler('xyz', wdt).as_quat()
+
+    # Derive the trajectory with the constant displacement
     t = 0.
-    p_txyz = [(t, np.zeros(3))]                 # [(time, position), ...]
-    q_txyzw = [(t, np.array([0., 0., 0., 1.]))] # [(time, orientation), ...]
-
-    displacement = np.array([speed * time_step, 0, 0])
-    while p_txyz[-1][-1][0] < length:
-        t += time_step
-        p_txyz.append((t, p_txyz[-1][-1] + displacement))
-        q_txyzw.append((t, q_txyzw[-1][-1].copy()))
-
+    p_txyz  = [(t, np.array(p_init))]                           # [(time, position), ...]
+    q_txyzw = [(t, R.from_euler('xyz', euler_init).as_quat())]  # [(time, orientation), ...]
+    distance = 0
+    while distance < length:
+        t += timestep
+        p_txyz.append((t, p_txyz[-1][-1] + hamilton_rotate(q_txyzw[-1][-1], vdt)))
+        q_txyzw.append((t, hamilton_product(q_txyzw[-1][-1], wdt)))
+        distance += np.linalg.norm(vdt)
     return p_txyz, q_txyzw
 
 
@@ -49,7 +75,7 @@ class DatasetPlayer:
 
 if __name__ == '__main__':
     # Generate synthetic data
-    true_pos, true_ori = gen_straight_line()
+    true_pos, true_ori = make_cv_trajectory(length=10, timestep=0.5, v=0.1, euler_init=(0, np.deg2rad(-10), 0))
     dataset = {
         'position':     add_gaussian_noise(true_pos),
         'orientation':  add_gaussian_noise(true_ori)}
