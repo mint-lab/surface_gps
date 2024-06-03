8418890a235ca29c19d0e9c6403fccccfe0fd11c
Sunglok Choi <sunglok@hanmail.net>
2024-06-04 08:50:12 +0900

    RENAME: Separate data generation codes to `synthetic_dataset.py`

---
+++ surface_gps/synthetic_dataset.py
@@ -0,0 +1,80 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+
+def conjugate(q_xyzw:np.array) -> np.array:
+    '''Return the conjugate of a quaternion q_xyzw = [x, y, z, w] as [-x, -y, -z, w]'''
+    return np.array([-q_xyzw[0], -q_xyzw[1], -q_xyzw[2], q_xyzw[3]])
+
+
+def hamilton_product(q1:np.array, q2:np.array) -> np.array:
+    '''Return the Hamilton product of two quaternions q1 and q2 as q1*q2 = [x, y, z, w]'''
+    return np.array([
+        q1[3]*q2[0] + q1[0]*q2[3] + q1[1]*q2[2] - q1[2]*q2[1],
+        q1[3]*q2[1] - q1[0]*q2[2] + q1[1]*q2[3] + q1[2]*q2[0],
+        q1[3]*q2[2] + q1[0]*q2[1] - q1[1]*q2[0] + q1[2]*q2[3],
+        q1[3]*q2[3] - q1[0]*q2[0] - q1[1]*q2[1] - q1[2]*q2[2]])
+
+
+def hamilton_rotate(q_xyzw:np.array, v_xyz:np.array) -> np.array:
+    '''Return the rotated vector v_xyz by the quaternion q_xyzw as a 3D vector'''
+    return hamilton_product(hamilton_product(q_xyzw, np.array([v_xyz[0], v_xyz[1], v_xyz[2], 0])), conjugate(q_xyzw))[:3]
+
+
+def make_cv_trajectory(length=100, timestep=1, v=1, w=0, p_init=[0, 0, 0], euler_init=[0, 0, 0]):
+    '''Return lists of time-stampled positions and orientations with a constant linear and angular velocity'''
+    # Calculate the displacement from the constant linear and angular velocity
+    if type(v) is float or type(v) is int:
+        v = [v, 0, 0] # Assume the 1D velocity is along the x-axis
+    if type(w) is float or type(w) is int:
+        w = [0, 0, w] # Assume the 1D angular velocity is around the z-axis
+    vdt = np.array(v) * timestep
+    wdt = np.array(w) * timestep
+    wdt = R.from_euler('xyz', wdt).as_quat()
+
+    # Derive the trajectory with the constant displacement
+    t = 0.
+    p_txyz  = [(t, np.array(p_init))]                           # [(time, position), ...]
+    q_txyzw = [(t, R.from_euler('xyz', euler_init).as_quat())]  # [(time, orientation), ...]
+    distance = 0
+    while distance < length:
+        t += timestep
+        p_txyz.append((t, p_txyz[-1][-1] + hamilton_rotate(q_txyzw[-1][-1], vdt)))
+        q_txyzw.append((t, hamilton_product(q_txyzw[-1][-1], wdt)))
+        distance += np.linalg.norm(vdt)
+    return {'position': p_txyz, 'orientation': q_txyzw}
+
+
+def add_gaussian_noise(true_data, mean=0., std_dev=0.1):
+    '''Return the noisy data with Gaussian noise added to the true data'''
+    noisy_data = []
+    for (t, data) in true_data:
+        noisy_data.append((t, data + np.random.normal(mean, std_dev, data.shape)))
+    return noisy_data
+
+
+class DatasetPlayer:
+    '''A dataset reader to play a dataset of time-stamped data with different types of data'''
+    def __init__(self, dataset):
+        '''A constructor'''
+        self._type = list(dataset.keys())
+        self._dataset = list(dataset.values())
+        self._cursor = np.zeros(len(self._dataset), dtype=int)
+
+    def reset(self):
+        '''Reset the cursor to the beginning of the dataset'''
+        self._cursor = np.zeros(len(self._dataset), dtype=int)
+
+    def get_next(self):
+        '''Return the next time-stamped data from the dataset'''
+        # Select the next data to play
+        next_times = [data[cursor][0] if cursor < len(data) else np.inf for (data, cursor) in zip(self._dataset, self._cursor)]
+        select = np.argmin(next_times)
+        if next_times[select] == np.inf:
+            return None, None, None
+
+        # Advance the cursor and return the selected data
+        type = self._type[select]
+        timestamp, data = self._dataset[select][self._cursor[select]]
+        self._cursor[select] += 1
+        return type, timestamp, data
--- surface_gps/test_localizer_synth.py
+++ surface_gps/test_localizer_synth.py
@@ -1,84 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial.transform import Rotation as R
+from synthetic_dataset import make_cv_trajectory, add_gaussian_noise, DatasetPlayer
 from simple_localizer import SimpleLocalizer
-
-
-def conjugate(q_xyzw:np.array) -> np.array:
-    return np.array([-q_xyzw[0], -q_xyzw[1], -q_xyzw[2], q_xyzw[3]])
-
-
-def hamilton_product(q1:np.array, q2:np.array) -> np.array:
-    return np.array([
-        q1[3]*q2[0] + q1[0]*q2[3] + q1[1]*q2[2] - q1[2]*q2[1],
-        q1[3]*q2[1] - q1[0]*q2[2] + q1[1]*q2[3] + q1[2]*q2[0],
-        q1[3]*q2[2] + q1[0]*q2[1] - q1[1]*q2[0] + q1[2]*q2[3],
-        q1[3]*q2[3] - q1[0]*q2[0] - q1[1]*q2[1] - q1[2]*q2[2]])
-
-
-def hamilton_rotate(q_xyzw:np.array, v_xyz:np.array) -> np.array:
-    return hamilton_product(hamilton_product(q_xyzw, np.array([v_xyz[0], v_xyz[1], v_xyz[2], 0])), conjugate(q_xyzw))[:3]
-
-
-def make_cv_trajectory(length=100, timestep=1, v=1, w=0, p_init=[0, 0, 0], euler_init=[0, 0, 0]):
-    # Calculate the displacement from the constant linear and angular velocity
-    if type(v) is float or type(v) is int:
-        v = [v, 0, 0] # Assume the 1D velocity is along the x-axis
-    if type(w) is float or type(w) is int:
-        w = [0, 0, w] # Assume the 1D angular velocity is around the z-axis
-    vdt = np.array(v) * timestep
-    wdt = np.array(w) * timestep
-    wdt = R.from_euler('xyz', wdt).as_quat()
-
-    # Derive the trajectory with the constant displacement
-    t = 0.
-    p_txyz  = [(t, np.array(p_init))]                           # [(time, position), ...]
-    q_txyzw = [(t, R.from_euler('xyz', euler_init).as_quat())]  # [(time, orientation), ...]
-    distance = 0
-    while distance < length:
-        t += timestep
-        p_txyz.append((t, p_txyz[-1][-1] + hamilton_rotate(q_txyzw[-1][-1], vdt)))
-        q_txyzw.append((t, hamilton_product(q_txyzw[-1][-1], wdt)))
-        distance += np.linalg.norm(vdt)
-    return p_txyz, q_txyzw
-
-
-def add_gaussian_noise(true_data, mean=0., std_dev=0.1):
-    noisy_data = []
-    for (t, data) in true_data:
-        noisy_data.append((t, data + np.random.normal(mean, std_dev, data.shape)))
-    return noisy_data
-
-
-class DatasetPlayer:
-    def __init__(self, dataset):
-        self._type = list(dataset.keys())
-        self._dataset = list(dataset.values())
-        self._cursor = np.zeros(len(self._dataset), dtype=int)
-
-    def reset(self):
-        self._cursor = np.zeros(len(self._dataset), dtype=int)
-
-    def get_next(self):
-        # Select the next data to play
-        next_times = [data[cursor][0] if cursor < len(data) else np.inf for (data, cursor) in zip(self._dataset, self._cursor)]
-        select = np.argmin(next_times)
-        if next_times[select] == np.inf:
-            return None, None, None
-
-        # Advance the cursor and return the selected data
-        type = self._type[select]
-        timestamp, data = self._dataset[select][self._cursor[select]]
-        self._cursor[select] += 1
-        return type, timestamp, data
 
 
 if __name__ == '__main__':
     # Generate synthetic data
-    true_pos, true_ori = make_cv_trajectory(length=10, timestep=0.5, v=0.1, euler_init=(0, np.deg2rad(-10), 0))
+    truth = make_cv_trajectory(length=10, timestep=0.5, v=0.1, euler_init=(0, np.deg2rad(-10), 0))
     dataset = {
-        'position':     add_gaussian_noise(true_pos),
-        'orientation':  add_gaussian_noise(true_ori)}
+        'position':     add_gaussian_noise(truth['position'], std_dev=[0.03, 0.03, 0.3]),
+        'orientation':  add_gaussian_noise(truth['orientation'], std_dev=0.01)}
 
     # Instantiate the localizer
     localizer = SimpleLocalizer()
@@ -102,8 +33,8 @@ if __name__ == '__main__':
         results['orientation'].append(q)
 
     # Plot the results
-    true_ts = np.array([time for time, _ in true_pos])
-    true_ps = np.array([data for _, data in true_pos])
+    true_ts = np.array([time for time, _ in truth['position']])
+    true_ps = np.array([data for _, data in truth['position']])
     data_ts = np.array([time for time, _ in dataset['position']])
     data_ps = np.array([data for _, data in dataset['position']])
     estm_ts = np.array(results['timestamp'])
@@ -121,7 +52,7 @@ if __name__ == '__main__':
 
     plt.figure()
     plt.plot(true_ts, true_ps[:, -1], 'g-', linewidth=2, label='Truth')
-    plt.plot(data_ts, data_ps[:, -1], 'b-', linewidth=2, label='Data')
+    plt.plot(data_ts, data_ps[:, -1], 'b.', linewidth=2, label='Data')
     plt.plot(estm_ts, estm_ps[:, -1], 'r-', linewidth=2, label='Estimated')
     plt.xlabel('Time [sec]')
     plt.ylabel('Z [m]')
