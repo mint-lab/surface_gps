0f8b170d6b0104911d8cec01c8764c39b8ec00b9
Heo, Dongwook <124948998+dongwookheo@users.noreply.github.com>
2024-06-12 16:30:40 +0900

    FIX: Correct install path

--- install_packages.sh
+++ install_packages.sh
@@ -6,6 +6,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install ros-$ROS_DISTRO-rviz-imu-plugin
 
 # Install sensor packages
+cd ../ # .../src
 git clone -b dev_4.0.6 --recursive https://github.com/stereolabs/zed-ros2-wrapper.git
 git clone -b humble https://github.com/ros-drivers/ros2_ouster_drivers.git
 git clone -b ros2 https://github.com/ros-drivers/nmea_navsat_driver.git
@@ -14,7 +15,7 @@ git clone https://github.com/mint-lab/mint_cart_ros.git
 git clone -b foxy-devel https://github.com/dongwookheo/ros2_humble_ublox_f9r.git
 
 # Fix myahrs_ros2_driver.cpp
-cd myahrs_ros2_driver/myahrs_ros2_driver/src
+cd surface_gps/myahrs_ros2_driver/myahrs_ros2_driver/src
 sed -i 's/this->declare_parameter(\("[a-z_]*"\))/this->declare_parameter(\1, 0.0)/g' ./myahrs_ros2_driver.cpp
 
 # Apply revised launch files
