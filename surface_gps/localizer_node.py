9286f375ce951919d7251e1dbd4d64d3ebd2afbf
dongwookheo <hdwook3918@gmail.com>
2024-06-17 10:36:32 +0900

    REFACTOR: Wrap the simple_localizer.py

--- surface_gps/localizer_node.py
+++ surface_gps/localizer_node.py
@@ -18,28 +18,29 @@ from numpy.linalg import inv
 from scipy.spatial.transform import Rotation
 from pyproj import Transformer
 
+from surface_gps.simple_localizer import SimpleLocalizer
+
 
 class EKF_node(Node):
-    def __init__(self):
-        super().__init__('ekf_node')
-        self.subscription = self.create_subscription(
-            NavSatFix,
-            '/ublox/fix',
-            self.fix_callback,
-            10)
-        
-        self.imu_subscription = self.create_subscription(
-            Imu,
-            '/imu/data',
-            self.imu_callback,
-            10
-        )
-        self.pub_tf = self.create_publisher(TFMessage, '/tf', 10)
-        self.pub_path = self.create_publisher(Path, '/ekf_path', 10)
-        self.pub_nav_path = self.create_publisher(Path, '/nav_path', 10)
+    """A ROS node for the Extended Kalman Filter (EKF) with GPS and IMU data(AHRS)"""
 
-        self.pub_ekf_pose = self.create_publisher(PoseStamped, '/ekf_pose', 10)
-        self.pub_nav_pose = self.create_publisher(PoseStamped, '/nav_pose', 10)
+    def __init__(self):
+        '''A constructor'''
+        super().__init__("ekf_node")
+        self.subscription = self.create_subscription(
+            NavSatFix, "/ublox/fix", self.gps_callback, 10
+        )
+
+        self.imu_subscription = self.create_subscription(
+            Imu, "/imu/data", self.imu_callback, 10
+        )
+
+        self.pub_tf = self.create_publisher(TFMessage, "/tf", 10)
+        self.pub_path = self.create_publisher(Path, "/ekf_path", 10)
+        self.pub_nav_path = self.create_publisher(Path, "/nav_path", 10)
+
+        self.pub_ekf_pose = self.create_publisher(PoseStamped, "/ekf_pose", 10)
+        self.pub_nav_pose = self.create_publisher(PoseStamped, "/nav_pose", 10)
 
         self.path_list = []
         self.path_msg = Path()
@@ -47,108 +48,71 @@ class EKF_node(Node):
         self.nav_list = []
         self.nav_msg = Path()
 
-        self.positions = np.empty((0,3))
-        self.init_east = 0
-        self.init_north = 0
-        self.init_alt = 0
-        self.relative_matrix = 0
-        self.is_fix_init = True
-        self.is_imu_init = True
+        self.simple_localizer = SimpleLocalizer()
 
-        self.init_quart = np.zeros(4)
-
-        self.localizer = ExtendedKalmanFilter(dim_x=7, dim_z=7)
-        self.localizer.F = np.eye(7)
-        self.localizer.Q = np.eye(7)*0.01
+        self.ekf = ExtendedKalmanFilter(dim_x=7, dim_z=7)
+        self.ekf.F = np.eye(7)
+        self.ekf.Q = np.eye(7) * 0.01
         self.h = lambda x: x
         self.H = lambda x: np.eye(7)
-        self.R = np.zeros((7,7))
+        self.R = np.zeros((7, 7))
 
-        self.x = 0
-        self.y = 0
-        self.z = 0
+    def gps_callback(self, msg: NavSatFix):
+        '''A callback function for the GPS data'''
+        header = msg.header
+        gps_data = (
+            (msg.latitude, msg.longitude, msg.altitude),
+            msg.position_covariance,
+        )
 
-    def fix_callback(self, data):
-        if (self.is_imu_init == False):
-            latitude = data.latitude
-            longitude = data.longitude
-            altitude = data.altitude
+        self.simple_localizer.apply_gps_data(gps_data)
+        self.R[0, 0] = gps_data[1][0]  # sigma_xx
+        self.R[1, 1] = gps_data[1][4]  # sigma_yy
+        self.R[2, 2] = gps_data[1][8]  # sigma_zz
 
-            self.R[0,0] = data.position_covariance[0]
-            self.R[1,1] = data.position_covariance[4]
-            self.R[2,2] = data.position_covariance[8]
+        p_xyz = self.simple_localizer.get_pose()[0]  # (x, y, z)
+        self.x, self.y, self.z = p_xyz
 
-            utm_e, utm_n = self.lat_lon_to_utm(latitude, longitude)
+    def imu_callback(self, msg: Imu):
+        '''A callback function for the IMU data(AHRS)'''
+        header = msg.header
+        imu_data = (
+            (
+                msg.orientation.x,
+                msg.orientation.y,
+                msg.orientation.z,
+                msg.orientation.w,
+            ),
+            msg.orientation_covariance,
+        )
 
-            # Set Initial Position
-            if (self.is_fix_init == True):
-                self.init_east = utm_e
-                self.init_north = utm_n
-                self.init_alt = altitude
-                self.is_fix_init=False
+        self.simple_localizer.apply_ahrs_data(imu_data)
+        self.R[3, 3] = imu_data[1][0]  # sigma_qxqx
+        self.R[4, 4] = imu_data[1][4]  # sigma_qyqy
+        self.R[5, 5] = imu_data[1][8]  # sigma_qzqz
 
-            # Compute Relative Position (N, W, Z) axis
-            diff_e = -(utm_e-self.init_east)
-            diff_n = utm_n-self.init_north
-            diff_a = altitude-self.init_alt
-            pos_array = np.array([diff_n, diff_e, diff_a])
+        q_xyzw = self.simple_localizer.get_pose()[1]  # (qx, qy, qz, qw)
+        self.qx, self.qy, self.qz, self.qw = q_xyzw
 
-            # Change (N, W, Z) axis to (X, Y, Z) axis
-            filtered_position = inv(self.relative_matrix) @ pos_array
-            # print(filtered_position)
-            self.x = filtered_position[0]
-            self.y = filtered_position[1]
-            self.z = filtered_position[2]
-
-    def lat_lon_to_utm(self, latitude, longitude):
-        transformer = Transformer.from_crs('EPSG:4326', 'EPSG:32752')  # EPSG:4326 = GPS 기준 좌표계, EPSG:32752 = 서울(52S) 기준 좌표계
-        utm_easting, utm_northing = transformer.transform(latitude, longitude)
-        return utm_easting, utm_northing
-
-    def imu_callback(self, data):
-        q_x = data.orientation.x
-        q_y = data.orientation.y
-        q_z = data.orientation.z
-        q_w = data.orientation.w
-
-        self.R[3,3] = data.orientation_covariance[0]
-        self.R[4,4] = data.orientation_covariance[4]
-        self.R[5,5] = data.orientation_covariance[8]
-
-        # Set Initial Heading
-        if (self.is_imu_init == True):
-            self.init_quart = np.array([q_w, q_x, q_y, q_z])
-            self.relative_matrix = Rotation.from_quat([q_x, q_y, q_z, q_w]).as_matrix()
-            # print(self.relative_matrix)
-            self.is_imu_init=False
-
-        # Compute Relative Orientaion
-        current_q = np.array([q_w, q_x, q_y, q_z])
-        delta_q = quat.qmult(quat.qinverse(self.init_quart), current_q)
-        self.ox = delta_q[1]
-        self.oy = delta_q[2]
-        self.oz = delta_q[3]
-        self.ow = delta_q[0]
-
-        # Predict & Update Position
-        z = np.array([[self.x, self.y, self.z, self.ox, self.oy, self.oz, self.ow]]).T
-        self.localizer.update(z, HJacobian=self.H, Hx=self.h)
-        self.localizer.predict()
-        # print(self.localizer.x)
-
+        # Predict and update pose
+        state = np.array(
+            [[self.x, self.y, self.z, self.qx, self.qy, self.qz, self.qw]]
+        ).T
+        self.ekf.update(state, HJacobian=self.H, Hx=self.h)
+        self.ekf.predict()
 
         # Publish TF
         msg = TransformStamped()
-        msg.header.stamp = data.header.stamp
-        msg.header.frame_id = 'odom'
-        msg.child_frame_id = 'base_link'
-        msg.transform.rotation.x = float(self.localizer.x[3])
-        msg.transform.rotation.y = float(self.localizer.x[4])
-        msg.transform.rotation.z = float(self.localizer.x[5])
-        msg.transform.rotation.w = float(self.localizer.x[6])
-        msg.transform.translation.x = float(self.localizer.x[0])
-        msg.transform.translation.y = float(self.localizer.x[1])
-        msg.transform.translation.z = float(self.localizer.x[2])
+        msg.header.stamp = header.stamp
+        msg.header.frame_id = "odom"
+        msg.child_frame_id = "base_link"
+        msg.transform.translation.x = float(self.ekf.x[0])
+        msg.transform.translation.y = float(self.ekf.x[1])
+        msg.transform.translation.z = float(self.ekf.x[2])
+        msg.transform.rotation.x = float(self.ekf.x[3])
+        msg.transform.rotation.y = float(self.ekf.x[4])
+        msg.transform.rotation.z = float(self.ekf.x[5])
+        msg.transform.rotation.w = float(self.ekf.x[6])
 
         tf_msg = TFMessage()
         tf_msg.transforms = [msg]
@@ -156,15 +120,15 @@ class EKF_node(Node):
 
         # Publish EKF_path
         pose_msg = PoseStamped()
-        pose_msg.header.stamp = data.header.stamp
-        pose_msg.header.frame_id = 'map'
-        pose_msg.pose.position.x = float(self.localizer.x[0])
-        pose_msg.pose.position.y = float(self.localizer.x[1])
-        pose_msg.pose.position.z = float(self.localizer.x[2])
-        pose_msg.pose.orientation.x = float(self.localizer.x[3])
-        pose_msg.pose.orientation.y = float(self.localizer.x[4])
-        pose_msg.pose.orientation.z = float(self.localizer.x[5])
-        pose_msg.pose.orientation.w = float(self.localizer.x[6])
+        pose_msg.header.stamp = header.stamp
+        pose_msg.header.frame_id = "map"
+        pose_msg.pose.position.x = float(self.ekf.x[0])
+        pose_msg.pose.position.y = float(self.ekf.x[1])
+        pose_msg.pose.position.z = float(self.ekf.x[2])
+        pose_msg.pose.orientation.x = float(self.ekf.x[3])
+        pose_msg.pose.orientation.y = float(self.ekf.x[4])
+        pose_msg.pose.orientation.z = float(self.ekf.x[5])
+        pose_msg.pose.orientation.w = float(self.ekf.x[6])
 
         self.path_list.append(pose_msg)
         # print(self.path_list)
@@ -176,15 +140,15 @@ class EKF_node(Node):
         self.pub_ekf_pose.publish(pose_msg)
 
         nav_msg = PoseStamped()
-        nav_msg.header.stamp = data.header.stamp
-        nav_msg.header.frame_id = 'map'
+        nav_msg.header.stamp = header.stamp
+        nav_msg.header.frame_id = "map"
         nav_msg.pose.position.x = float(self.x)
         nav_msg.pose.position.y = float(self.y)
         nav_msg.pose.position.z = float(self.z)
-        nav_msg.pose.orientation.x = float(self.ox)
-        nav_msg.pose.orientation.y = float(self.oy)
-        nav_msg.pose.orientation.z = float(self.oz)
-        nav_msg.pose.orientation.w = float(self.ow)
+        nav_msg.pose.orientation.x = float(self.qx)
+        nav_msg.pose.orientation.y = float(self.qy)
+        nav_msg.pose.orientation.z = float(self.qz)
+        nav_msg.pose.orientation.w = float(self.qw)
 
         self.nav_list.append(nav_msg)
         # print(self.path_list)
@@ -195,6 +159,7 @@ class EKF_node(Node):
         # Publish Nav_pose
         self.pub_nav_pose.publish(nav_msg)
 
+
 def main():
     rclpy.init()
     node = EKF_node()
@@ -203,5 +168,6 @@ def main():
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
