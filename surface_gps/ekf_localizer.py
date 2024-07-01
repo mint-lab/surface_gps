0fb36e4949753c03a0df4f8f466721272264de99
dongwookheo <hdwook3918@gmail.com>
2024-07-01 17:34:03 +0900

    FEAT: Add test function to optimize barometric data

--- surface_gps/ekf_localizer.py
+++ surface_gps/ekf_localizer.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from filterpy.kalman import ExtendedKalmanFilter
 from filterpy.stats import plot_covariance
+from collections import deque
 
 """
 TODO:
@@ -21,6 +22,8 @@ class EKFLocalizer(ExtendedKalmanFilter):
         w_noise_var=5.895184e-06,
         q_noise_var=4.592449e-06,
         dt=0.1,
+        T0=20,
+        buffer_size=100,
     ):
         """
         @brief A Constructor
@@ -36,14 +39,26 @@ class EKFLocalizer(ExtendedKalmanFilter):
         """
         super().__init__(dim_x=10, dim_z=6)
         self.motion_noise = np.array([[v_noise_var, 0], [0, w_noise_var]])
-        self.h_pos_vel = lambda x: x[0:6]
-        self.h_quat = lambda x: x[6:10]
+        self.h_pos_vel = lambda x: x[0:6]  # x, y, z, vx, vy, vz
+        self.h_quat = lambda x: x[6:10]  # qx, qy, qz, qw
+        self.h_alt = lambda x: x[2]  # z
         # TODO: model jacobian matrices considering the lever-arm effect
         self.H_pos_vel = lambda x: np.eye(6, 10)
         self.H_quat = lambda x: np.eye(4, 10)
+        self.H_alt = lambda p: np.array([[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]])
         self.R_pos_vel = np.eye(6) * 0.01  # TODO: modify when wrapping with ROS
         self.R_quat = q_noise_var * np.eye(4)
+        # TODO: modify to altitude with error propagation R(p_noise_var, t_noise_var)
+        self.R_alt = np.array([[1]])
         self.dt = dt
+
+        self.T0 = T0 + 273.15  # K #! dummy
+        self.Tgrad = -0.0065  # K/m
+        self.g = 9.80665  # m/s^2
+        self.Gc = 287.058  # J/(kg*K)
+        self.P0 = None  # Pa
+
+        self.h_buffer = deque([], maxlen=buffer_size)
 
     def predict(self):
         """Predict the state"""
@@ -100,3 +115,18 @@ class EKFLocalizer(ExtendedKalmanFilter):
         elif measurement_type == "quat":
             self.R = self.R_quat
             super().update(z, HJacobian=self.H_quat, Hx=self.h_quat, R=self.R)
+
+    def calculate_altitude(self, P: float) -> bool:
+        """Calculate the altitude"""
+        if self.P0 is None:
+            self.P0 = P
+        h = (self.T0 / self.Tgrad) * (
+            1 - (P / self.P0) ** (self.Tgrad * self.Gc / self.g)
+        )
+        self.h_buffer.append(h)
+        return True
+
+    def optimize_altitude(self, h_buffer: deque) -> float:
+        """Optimize the altitude"""
+        mean = np.mean(h_buffer)
+        return mean
